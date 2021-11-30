d9ad006c1946fedba2e7c344b71c9b1fbce23989
Muse <kaydendeason@gmail.com>
2021-11-30 14:44:36 -0500

    Inital

---
+++ embed/remote.js
@@ -0,0 +1,4 @@
+window.tixEmbed = window.tixEmbed || {'inst':[]};
+window.tixEmbed.baseUrl = "https:\/\/mhsmi.hometownticketing.com";
+if(!window.tixEmbed.djsLoaded) { (function(){var s = document.getElementsByTagName("script")[0]; var b = document.createElement("script"); b.type = "text/javascript"; b.async = true; b.src = "https://mhsmi.hometownticketing.com/assets/js/remote_embed-2.0.1.js"; s.parentNode.insertBefore(b, s);})(); }
+window.tixEmbed.djsLoaded = true;
